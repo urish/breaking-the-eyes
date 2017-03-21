a80cf8d5dfaad5ab4913abae4f2f2f0c3242b72e
Uri Shaked <uri@salsa4fun.co.il>
2017-03-22 01:58:00 +0200

    implement the voter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { VoterComponent } from './voter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VoterComponent
   ],
   imports: [
     BrowserModule,
