fc0bec22918edab194702c8ffb20c31bde6207a6
Uri Shaked <uri@salsa4fun.co.il>
2017-11-06 22:38:03 +0000

    feat: vote with keyboard

--- src/app/voter/voter.component.ts
+++ src/app/voter/voter.component.ts
@@ -21,4 +21,11 @@ export class VoterComponent implements OnInit {
   downvote() {
     this.votes--;
   }
+
+  voteKeyDown($event: KeyboardEvent, vote: number) {
+    const key = $event.which;
+    if (key === 13 || key === 32) {
+      this.votes += vote;
+    }
+  }
 }
